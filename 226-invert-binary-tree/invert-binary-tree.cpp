519529ed53cb38993054c0cf25ded5d946309865
vedu989v-wq <vedu989v@gmail.com>
2026-03-18 01:57:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (25.43%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void invert(TreeNode *root){
+        //base condition
+        if(root==NULL)
+        return;
+
+        TreeNode *temp=root->right;
+        root->right=root->left;
+        root->left=temp;
+        
+        //Recursive
+        invert(root->left);
+        invert(root->right);
+
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        invert(root);
+        return root;
+    }
+};
