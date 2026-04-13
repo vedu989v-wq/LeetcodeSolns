95538547f1edd874b18d6e5ae4b736552f76e203
Vedansh Sharma <vedu989v@gmail.com>
2026-04-13 20:33:44 +0530

    Time: 3 ms (69.75%) | Memory: 13 MB (89.14%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,52 +1,30 @@
 class Solution {
 public:
-//theek thaak approach, not so good after all
-   
-    void permutationofstring(int n, vector<string>&v, string &current, int open, int close){
-        if(open == 0 && close == 0 || n==0){
-            
-            v.push_back(current); //Mistake 1: push is done in the base case, not below the recursive step
-            return; 
-        }
-        
-        for(int i=0; i<2; i++){
-            if(i==0){
-            if(open>0){
-            current=current+'(';
-            permutationofstring(n-1, v, current, open-1, close);
-            
-            current.pop_back(); //Mistake 2: pop is necessary for back track and removal of lastly added paranthesis..., at each step, not after pushing back, and after recursive steps.
-            }
-            }
-            else{
-            if(close>open){
-            current=current+')';
-            permutationofstring(n-1, v, current, open, close-1);
-            //Mistake 2: pop is necessary for back track and removal of lastly added paranthesis..., at each step, not after pushing back, and after recursive steps.
-            current.pop_back();
-            }
-            }
-            
-        }
-          // ❌ This line was WRONG:
-        // v.push_back(current);
-        // Reason:
-        // - It was adding incomplete/invalid strings
-        // - Results should ONLY be added at base case
-
-        // ❌ This pop_back was misplaced
-        // current.pop_back();
-        // - Backtracking must happen INSIDE each branch, not once at the end
-        
-        
-        
+void permutationofstring(vector<string>& v, string &current, int open, int close){
+    
+    if(open == 0 && close == 0){
+        v.push_back(current);
+        return;
     }
-
-    vector<string> generateParenthesis(int n) {
-        vector<string>v;
-        string current="";
-        permutationofstring(n*2, v, current, n, n);
-        
-     return v;
+    
+    // add '('
+    if(open > 0){
+        current.push_back('(');
+        permutationofstring(v, current, open-1, close);
+        current.pop_back();
     }
+    
+    // add ')'
+    if(close > open){
+        current.push_back(')');
+        permutationofstring(v, current, open, close-1);
+        current.pop_back();
+    }
+}
+   vector<string> generateParenthesis(int n) {
+    vector<string> v;
+    string current = "";
+    permutationofstring(v, current, n, n);
+    return v;
+}
 };
