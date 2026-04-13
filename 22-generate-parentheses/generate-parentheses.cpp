2423598c82849febeca3f41efbbd0de393bbf732
Vedansh Sharma <vedu989v@gmail.com>
2026-04-13 20:27:02 +0530

    Time: 3 ms (69.75%) | Memory: 15.5 MB (63.39%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    bool check(string str){
-        stack<char>s;
-        for(int i=0; i<str.size(); i++){
-            if(str[i]=='('){
-                s.push(str[i]);
-            }
-            else{
-                if(s.empty())
-                return 0;
-                else
-                s.pop();
-            }
-        }
-        return s.empty();
-    }
-    void permutationofstring(int n, vector<string>&v, string &current){
-        if(n==0){
-            if(check(current))
+//theek thaak approach, not so good after all
+   
+    void permutationofstring(int n, vector<string>&v, string &current, int open, int close){
+        if(open == 0 && close == 0 || n==0){
+            
             v.push_back(current); //Mistake 1: push is done in the base case, not below the recursive step
             return; 
         }
         
         for(int i=0; i<2; i++){
             if(i==0){
+            if(open>0){
             current=current+'(';
-            permutationofstring(n-1, v, current);
+            permutationofstring(n-1, v, current, open-1, close);
+            
             current.pop_back(); //Mistake 2: pop is necessary for back track and removal of lastly added paranthesis..., at each step, not after pushing back, and after recursive steps.
             }
+            }
             else{
+            if(close>open){
             current=current+')';
-            permutationofstring(n-1, v, current);//Mistake 2: pop is necessary for back track and removal of lastly added paranthesis..., at each step, not after pushing back, and after recursive steps.
+            permutationofstring(n-1, v, current, open, close-1);
+            //Mistake 2: pop is necessary for back track and removal of lastly added paranthesis..., at each step, not after pushing back, and after recursive steps.
             current.pop_back();
+            }
             }
             
         }
@@ -52,7 +45,7 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string>v;
         string current="";
-        permutationofstring(n*2, v, current);
+        permutationofstring(n*2, v, current, n, n);
         
      return v;
     }
