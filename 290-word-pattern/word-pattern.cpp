fd448e7d6ce97cccdae1c09a01ab7a86c3861464
Vedansh Sharma <vedu989v@gmail.com>
2026-04-17 18:32:40 +0530

    Time: 2 ms (15.19%) | Memory: 8.9 MB (40.15%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,40 @@
+#include <sstream>
+#include <string>
+
+class Solution {
+public:
+
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, string>m;
+        unordered_set<string> used;
+        vector<string>v;
+        string segment;
+        stringstream ss(s);
+      
+        while (ss >> segment) { //  CHANGED
+            v.push_back(segment);
+        }
+
+    //  ADDED (size check to avoid out-of-bounds)
+        if (pattern.length() != v.size())
+         return false;
+
+    for(int i=0; i<pattern.length(); i++){
+         if(m.find(pattern[i]) != m.end()){
+                if(m[pattern[i]] != v[i]) 
+                return false;
+            }
+            else{
+                // ADDED: ensure bijection (no two chars map to same word)
+                if(used.count(v[i]))
+                return false;
+
+                m[pattern[i]] = v[i];
+                used.insert(v[i]); //  ADDED
+            }
+    }
+    
+    
+    return true;
+    }
+};
