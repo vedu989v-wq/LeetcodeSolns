2c80297062163452618614be3b33389a4089647c
Vedansh Sharma <vedu989v@gmail.com>
2026-05-09 21:43:49 +0530

    Time: 267 ms (18.93%) | Memory: 213.3 MB (5.12%) - LeetSync

---
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        unordered_map<int, int>m;
+        for(int i=0; i<banned.size(); i++){
+            m[banned[i]]=-1;
+        }
+
+        vector<int>arr;
+        for(int i=1; i<=n; i++){
+           if(m[i]!=-1){
+             arr.push_back(i);
+           }
+        }
+
+        //simple sa counting logic implement kar sakte hai idhar
+        int sum=0;
+        int count=0;
+
+        for(int i=0; i<arr.size(); i++){
+            if(sum+arr[i]<=maxSum){
+                sum+=arr[i];
+                count++;
+            }
+            else
+            break;
+        }
+
+        
+        return count;
+    }
+};
