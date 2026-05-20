2a2478daf9337ccb72b04b4d4350e740441bae6a
Vedansh Sharma <vedu989v@gmail.com>
2026-05-20 19:53:34 +0530

    Time: 1 ms (77.24%) | Memory: 85.8 MB (85.47%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-       //module 1:
-       
-        vector<int>prefix;
-        unordered_map<int , int>m1;
-        unordered_map<int , int>m2;
-        for(int i=0; i<A.size(); i++){
-            int com=0;
-            m1[A[i]]=1;
-            m2[B[i]]=1;
-        
-        for (const auto& [key, value] : m1) {
-        
-        if(m2.count(key)){
-            com++;
-        }
-        
-    }
-    prefix.push_back(com);
-}
-//    reverse(prefix.begin(), prefix.end());
-   return prefix;
+      //optimal (frequency array method)
+      int n=A.size();
+      vector<int>prefix(n), frequency(n+1, 0);
+      int commonCount=0;
+      //checking phase, iterating through the elements of both arrays
+
+      for(int i=0; i<n; i++){
+        //incrementing frequencies of current elements of A and B, if 2 reaches, we found a common elements
+
+        //checking if the element in A has appeared before(common in prefix);
+        if(++frequency[A[i]]==2)
+        commonCount++;
+        //same process for Array B
+        if(++frequency[B[i]]==2)
+        commonCount++;
+
+        //storing phase
+        prefix[i]=commonCount;
+      }
+
+      return prefix;
    }
 };
