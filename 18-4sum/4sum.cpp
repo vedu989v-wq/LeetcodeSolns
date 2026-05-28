bc3cda73d0fa72f92fa4641a7fcd62e6d0908003
Vedansh Sharma <vedu989v@gmail.com>
2026-05-28 18:20:57 +0530

    Time: 15 ms (90.66%) | Memory: 17.7 MB (11.99%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,23 +4,29 @@ public:
       //if size of nums is less than 4
       if(nums.size()<4)
       return {};
-
+     
+     //step 1: sorting the array for 2 pointers
        sort(nums.begin(), nums.end());
        vector<vector<int>>v;
 
-
+     //predeciding the range of search for outer loop
        for(int i=0; i<nums.size()-3;i++ ){
-
+        
+        //duplicacy handled for outer loop
         if(i > 0 && nums[i] == nums[i-1])   
                 continue;
-
+     //predeciding the range for inner loop 
         for(int j=i+1; j<nums.size()-2; j++){
+
+            //duplicacy handled for inner loop
             if(j>i+1 && nums[j]==nums[j-1]){
                 continue;
             }
 
             long long ans=(long long)target-nums[i]-nums[j];
             int start=j+1, end=nums.size()-1;
+
+            //two pointers 
             while(start<end){
                 long long sum=nums[start]+nums[end];
                 if(sum==ans){
@@ -33,7 +39,8 @@ public:
                 v.push_back(v1);
                 start++; 
                 end--;
-
+                
+                //duplicacy handled for while loop
                 while(start < end && nums[start] == nums[start-1])
                         start++;
 
