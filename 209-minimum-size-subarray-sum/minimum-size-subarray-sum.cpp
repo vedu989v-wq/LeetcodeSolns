776b03670361944261ea89f81a9c4f09fe0772d6
Vedansh Sharma <vedu989v@gmail.com>
2026-06-09 12:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (40.29%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-         int mini=INT_MAX;
+        int mini = INT_MAX;
 
-         //step 1: pehla subarray
-         int fsum=0;
-         int f_idx=-1;
+        int fsum = 0;
+        int p_idx = 0;
 
-         for(int i=0; i<nums.size(); i++){
-          fsum=fsum+nums[i];
-          if(fsum>=target){
-            f_idx=i;
-            break;
-          }
-          }
-         
-         if(f_idx==-1)
-         return 0;
+        for (int f_idx = 0; f_idx < nums.size(); f_idx++) {
 
-         //step 2: finding minimum subarray
-         int p_idx=0;
+            fsum += nums[f_idx];
 
-         while(f_idx<nums.size()){
-            if(fsum>=target){
-             mini=min(mini, f_idx-p_idx+1);
-             fsum=fsum-nums[p_idx];
-             p_idx++;
+            while (fsum >= target) {
+                mini = min(mini, f_idx - p_idx + 1);
+
+                fsum -= nums[p_idx];
+                p_idx++;
             }
-            else{
-                
-                f_idx++;
-                if(f_idx<nums.size())
-                fsum+=nums[f_idx];
-            }
-            
-         }
+        }
 
-         
-         return mini;
+        return (mini == INT_MAX) ? 0 : mini;
     }
 };
