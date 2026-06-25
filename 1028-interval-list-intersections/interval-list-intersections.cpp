6d6124940ddc6f358d48a2f67cd2396131760925
Vedansh Sharma <vedu989v@gmail.com>
2026-06-25 16:51:15 +0530

    Time: 11 ms (8.18%) | Memory: 27.7 MB (5.99%) - LeetSync

---
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+
+    firstList.reserve(firstList.size() + secondList.size());
+    firstList.insert(firstList.end(), secondList.begin(), secondList.end());
+
+    vector<vector<int>>intervals=firstList;
+    sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    });
+
+     vector<vector<int>>ans;
+     vector<vector<int>>isec;
+
+       for(int i=0; i<intervals.size()-1; i++){
+        while(i<intervals.size()-1 && intervals[i][0]<=intervals[i+1][0] && intervals[i+1][0]<=intervals[i][1]){
+            if(intervals[i][0]<=intervals[i+1][1] && intervals[i+1][1]<=intervals[i][1]){
+                isec.push_back({intervals[i+1][0], intervals[i+1][1]});
+                intervals[i+1]={intervals[i][0], intervals[i][1]};
+            }
+            else{
+               isec.push_back({intervals[i+1][0], intervals[i][1]});
+               intervals[i+1]={intervals[i][0], intervals[i+1][1]};
+               
+               }
+
+            i++;
+        }
+        ans.push_back(intervals[i]);
+
+        if(i==intervals.size()-2)
+        ans.push_back(intervals[i+1]);
+       }
+
+       return isec;
+  }
+};
