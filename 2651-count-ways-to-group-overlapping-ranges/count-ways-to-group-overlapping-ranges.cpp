7b35d824d5a225131067bc4907fdc530c1162fda
Vedansh Sharma <vedu989v@gmail.com>
2026-06-28 15:03:12 +0530

    Time: 29 ms (87.59%) | Memory: 77 MB (59.12%) - LeetSync

---
+++ 2651-count-ways-to-group-overlapping-ranges/count-ways-to-group-overlapping-ranges.cpp
@@ -0,0 +1,39 @@
+#include<cmath>
+class Solution {
+public:
+
+    int countWays(vector<vector<int>>& intervals) {
+        int count=0;
+        if(intervals.size()==1)
+        count++;
+        
+
+        long long mod=1e9+7; //modulo
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    });
+
+        
+
+        for(int i=0; i<intervals.size()-1; i++){
+        while(i<intervals.size()-1 && intervals[i][0]<=intervals[i+1][0] && intervals[i+1][0]<=intervals[i][1]){
+            if(intervals[i][0]<=intervals[i+1][1] && intervals[i+1][1]<=intervals[i][1]){
+                intervals[i+1]={intervals[i][0], intervals[i][1]};
+            }
+            else
+               intervals[i+1]={intervals[i][0], intervals[i+1][1]};
+
+            i++;
+        }
+        count++;
+
+        if(i==intervals.size()-2)
+        count++;
+       }
+       int result=1;
+       for(int i=0; i<count; i++){
+        result=(result*2)%mod;
+       }
+       return result;
+    }
+};
