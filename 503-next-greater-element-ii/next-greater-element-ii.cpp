a49aeccad111153f50804872e8387a7bec9993ad
Vedansh Sharma <vedu989v@gmail.com>
2026-07-29 22:49:17 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (5.34%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,26 +1,40 @@
-class Solution {
+class Solution
+{
 public:
+    vector<int> NGT(vector<int> &arr)
+    {
+        stack<int> st;
+        vector<int> ans(arr.size(), -1);
 
- vector<int>NGT(vector<int> &arr){
-      stack<int>st;
-        vector<int>ans(arr.size(), -1);
-        int size=arr.size();
-
-        for(int i=2*arr.size()-1; i>=0; i--){
-         while(!st.empty() && arr[st.top()]<=arr[i%size]){
-            st.pop();
-         }
-         if(!st.empty()){
-            ans[i%size]=arr[st.top()];
-         }
-         st.push(i%size);
+        for (int i = arr.size() - 1; i >= 0; i--)
+        {
+            while (!st.empty() && arr[st.top()] <= arr[i])
+            {
+                st.pop();
+            }
+            if (!st.empty())
+            {
+                ans[i] = arr[st.top()];
+            }
+            st.push(i);
         }
 
         return ans;
     }
 
-    vector<int> nextGreaterElements(vector<int>& nums) {
-       vector<int>ans=NGT(nums);
-       return ans;
+    vector<int> nextGreaterElements(vector<int> &nums)
+    {
+        // concatenating same arrays two times
+        //  1. Reserve the combined size upfront to prevent multiple memory reallocations
+        vector<int> ans = nums;
+        ans.reserve(nums.size() + nums.size());
+
+        // 2. Append the second array as a single block copy
+        ans.insert(ans.end(), nums.begin(), nums.end());
+
+        ans = NGT(ans);
+        ans.resize(nums.size());
+        return ans;
+        //3. if not using reserve, stl implementation doubling method, will take a lil more time...
     }
 };
